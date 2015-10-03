5b0a953e51ce3ad685549f697aa2555b26b70b94
zakihaya <toru.hayazaki@gmail.com>
2015-10-03 08:36:37 +0000

    create login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'signup', to: 'users#new'
+  get 'signup',  to: 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
